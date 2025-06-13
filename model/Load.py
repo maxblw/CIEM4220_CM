f0140f38411fcd4b4513b5dfafe5957e6b9f5615
Max Blankwater <m.a.blankwater@student.tudelft.nl>
2025-06-13 11:50:19 +0200

    commit

--- model/Load.py
+++ model/Load.py
@@ -12,6 +12,8 @@ def goda_original(h, beta, Hmax, eta, k):
     rho = density of the water
     eta = surface elevation
     """
+    if eta < 0:
+        return [0, 0] # no load if wave is retreating
     rho = 1025 # density of salt water
     g = 9.81 #m/s^2
 
@@ -23,7 +25,7 @@ def goda_original(h, beta, Hmax, eta, k):
     alpha2 = min(((h_b - h) / (3* h_b)) * (Hmax / h)**2, (2*h) / Hmax)
     alpha3 = 1 - (h_ / h) * (1 - (1 / np.cosh(k * h)))
    
-    p1 = 1/2 * (np.cos(beta) + 1) * (alpha1 + alpha2 *(np.cos(beta)) ** 2) * rho * g * Hmax
+    p1 = 1/2 * (np.cos(beta) + 1) * (alpha1 + alpha2 *(np.cos(beta)) ** 2) * rho * g * eta
     p3 = alpha3 * p1
     return [p1, p3]
 
@@ -42,8 +44,7 @@ def array_Goda(h_tot, h1, h2, eta, goda, k, H, printing=False):
     p1 = goda[0]
     # p2 = goda[1]
     p3 = goda[1]
-    d = eta + h1
-
+    h_bed = eta + h1
     z = np.linspace(0, h_tot, 1000) # z coordinate from bottom to top of the caisson
 
     # define the correction factors
@@ -54,7 +55,7 @@ def array_Goda(h_tot, h1, h2, eta, goda, k, H, printing=False):
     p2til = 0.45 * p3til
     p1til = 0
     if printing == True:
-        print("h1", h1, "h2", h2, "d", d)
+        print("h1", h1, "h2", h2, "d", h_bed)
         print("p1", p1, "p3", p3)
         print("p1til", p1til, "p2til", p2til, "p3til", p3til)
     # Define the pressure distribution 
@@ -64,7 +65,7 @@ def array_Goda(h_tot, h1, h2, eta, goda, k, H, printing=False):
     Pr = np.zeros(len(z))  # Initialize corrected pressure array
     ptil = np.zeros(len(z))  # Initialize pressure correction array
 
-    if d > h2:
+    if h_bed > h2:
         p4 = p1 * (h2 - h1) / (eta)
         Pv[np.where(z < h1)] = np.linspace(p3, p1, np.sum(z < h1))  # linear interpolation from P1 to P2
         Pv[np.where(z >= h1)] = np.linspace(p1, p4, np.sum(z >= h1))  # linear interpolation from P2 to P3
@@ -91,9 +92,9 @@ def array_Goda(h_tot, h1, h2, eta, goda, k, H, printing=False):
             plt.grid(True)
             plt.legend()
 
-    if d < h2:
+    if h_bed < h2:
         Pv[np.where(z < h1)] = np.linspace(p3, p1, np.sum(z < h1))
-        Pv[np.where((z >= h1) & (z < d))] = np.linspace(p1, 0, len(Pv[np.where((z >= h1) & (z < d))]))
+        Pv[np.where((z >= h1) & (z < h_bed))] = np.linspace(p1, 0, len(Pv[np.where((z >= h1) & (z < h_bed))]))
         # print("Pv", np.linspace(p1, 0, len(np.where((z >= h1) & (z < d)))))
         Pr = Pv.copy()  # No correction for this case
 
